61b563050b216b7b71f5cf5c99ab497fc1dbd980
Qtae <qtkim91@gmail.com>
2021-10-12 16:25:28 +0900

    add shuffle function, exe single instance

--- hanagram.py
+++ hanagram.py
@@ -1,10 +1,13 @@
 #main controller of the program.
 import os
 import sys
+import random
+
+from tendo import singleton
 
 from PyQt5 import uic
 from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QScrollBar
 
 from permutation import Permutation
 
@@ -32,24 +35,37 @@ class HanagramWindow(QMainWindow, hanagram_ui):
   def __init__(self, QApplication):
     super().__init__()
     self.setupUi(self)
-    self.setWindowTitle('hanagram ver 0.0.4')
+    self.setWindowTitle('hanagram ver 0.0.5')
 
     self.permut = Permutation()
     self.string = ''
+    self.res = []
 
     self.pushButton.clicked.connect(self._clicked_button)
+    self.pushButton_Shuffle.clicked.connect(self._clicked_shuffle_button)
     self.lineEdit.textEdited.connect(lambda: self._edited_line_text(self.lineEdit.text()))
   
   def _clicked_button(self):
-    self.textBrowser.clear()
-    res = self.permut.get_permutation_result(self.string)
-    for res_string in res:
-      self.textBrowser.append(res_string)
+    self.res = self.permut.get_permutation_result(self.string)
+    self._update_textedit()
+
+  def _clicked_shuffle_button(self):
+    if self.res == []: return
+    random.shuffle(self.res)
+    self._update_textedit()
   
   def _edited_line_text(self, text):
     self.string = text
 
+  def _update_textedit(self):
+    self.textBrowser.clear()
+    for res_string in self.res:
+      self.textBrowser.append(res_string)
+    self.textBrowser.verticalScrollBar().triggerAction(QScrollBar.SliderToMinimum)
+    
+
 if __name__=='__main__':
+  me = singleton.SingleInstance()
   app = QApplication(sys.argv)
   win = HanagramWindow(app)
   win.show()
